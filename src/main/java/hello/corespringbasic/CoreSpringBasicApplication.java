6fa0d8b050a7eb8427e9ac570c97c673f2b72c1b
SangWook <imcool2551@gmail.com>
2022-01-22 15:20:26 +0900

    first commit

---
+++ src/main/java/hello/corespringbasic/CoreSpringBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.corespringbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreSpringBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreSpringBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/corespringbasic/CoreSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.corespringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreSpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
