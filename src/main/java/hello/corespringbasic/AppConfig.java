4cda1c147314c5a51d94a05a77ebb0f6716c0239
SangWook <imcool2551@gmail.com>
2022-01-23 10:43:26 +0900

    AppConfig 순수자바 컨테이너에에서 스프링 컨테이너로 변경

--- src/main/java/hello/corespringbasic/AppConfig.java
+++ src/main/java/hello/corespringbasic/AppConfig.java
@@ -1,29 +1,36 @@
 package hello.corespringbasic;
 
 import hello.corespringbasic.discount.DiscountPolicy;
-import hello.corespringbasic.discount.FixDiscountPolicy;
+import hello.corespringbasic.discount.RateDiscountPolicy;
 import hello.corespringbasic.member.MemberService;
 import hello.corespringbasic.member.MemberServiceImpl;
 import hello.corespringbasic.member.MemoryMemberRepository;
 import hello.corespringbasic.order.OrderService;
 import hello.corespringbasic.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+    @Bean
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
--- src/main/java/hello/corespringbasic/MemberApp.java
+++ src/main/java/hello/corespringbasic/MemberApp.java
@@ -3,12 +3,17 @@ package hello.corespringbasic;
 import hello.corespringbasic.member.Grade;
 import hello.corespringbasic.member.Member;
 import hello.corespringbasic.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
